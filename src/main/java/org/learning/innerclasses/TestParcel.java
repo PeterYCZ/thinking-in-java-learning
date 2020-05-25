efd23e1d58d54e78a5f95b9b1089674372bc539f
PeterYCZ <yecongzhoupeter@qq.com>
2020-05-25 15:47:44 +0800

    使用.this与.new

--- src/main/java/org/learning/innerclasses/TestParcel.java
+++ src/main/java/org/learning/innerclasses/TestParcel.java
@@ -35,7 +35,7 @@ public class TestParcel {
        Parcel4 p = new Parcel4();
        Contents c = p.contents();
        Destination d = p.destination("Tasmania");
-       //Illegal -- can't access provate class:
+       //Illegal -- can't access private class:
         //! Parcel4.PContents pc = p.new PContents();
     }
 }
