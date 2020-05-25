e3cff6394f9b4b527dee623a626d5c857f610af9
PeterYCZ <yecongzhoupeter@qq.com>
2020-05-25 16:02:06 +0800

    局部内部类 - 只能在声明的作用域中使用

---
+++ src/main/java/org/learning/innerclasses/Parcel6.java
@@ -0,0 +1,30 @@
+package org.learning.innerclasses;
+
+//Nesting a class within a scope.
+
+public class Parcel6 {
+    private void internalTracking(boolean b) {
+        if(b) {
+            class TrackingSlip {
+                private String id;
+                TrackingSlip(String s) {
+                    id = s;
+                }
+                String getSlip() {
+                    return id;
+                }
+            }
+        }
+    }
+
+    //Can't use it here! Out of scope
+    //! TrackingSlip ts = new TrackingSlip("slip")
+    public void track() {
+        internalTracking(true);
+    }
+
+    public static void main(String[] args) {
+        Parcel6 p = new Parcel6();
+        p.track();
+    }
+}
