0b622508ab99b94264c1ec25c4ad5e844fa0fdf9
PeterYCZ <yecongzhoupeter@qq.com>
2020-08-10 16:08:56 +0800

    枚举类 -- 状态机

---
+++ src/main/java/org/learning/Input.java
@@ -0,0 +1,38 @@
+package org.learning;
+
+import netscape.security.UserTarget;
+
+import java.util.Random;
+
+public enum Input {
+
+    NICKEL(5), DIME(10),QUARTER(25),DOLLER(100),
+    TOOTHPASTE(200),CHIPS(75),SODA(100),SOAP(50),
+
+    ABORT_TRANSACTION {
+         @Override
+         public int amount() {
+             throw new RuntimeException("ABORT.amount()");
+         }
+    },
+
+    STOP {
+        @Override
+        public int amount() {
+            throw new RuntimeException("ABORT.amount()");
+        }
+    };
+
+    int value;
+    Input(int value) {
+        this.value =value;
+    }
+    Input() {}
+    int amount() {
+        return value;
+    }
+    static Random rand = new Random(47);
+    public static Input randomSelection() {
+        return values()[rand.nextInt(values().length - 1)];
+    }
+}
